adfddcc5006d118c992fcc856e62942a11c442fb
Korolenko Daria <admem75213@gmail.com>
2024-04-03 21:53:18 +0300

    test  git commit -m test
    git commit -m test
    git checkout -b Korolenko_fix_files

--- routes/waterRateRoutes.js
+++ routes/waterRateRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { updateWaterRate } from "../controllers/waterRateControllers.js";
-import userSchemaWaterRate from "../shemas/waterRateSchema.js";
+import userSchemaWaterRate from "../schemas/waterRateSchema.js";
 import validateBody from "../helpers/validateBody.js";
 import { authenticate } from "../middlewares/auth.js";
 
