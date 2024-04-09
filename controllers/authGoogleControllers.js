e48617604ea72ecfd4b369ed4021d7ee33390d13
Yuliya Turasova <turasova1986@gmail.com>
2024-04-09 10:11:12 +0300

    fix auth and login

--- controllers/authGoogleControllers.js
+++ controllers/authGoogleControllers.js
@@ -32,17 +32,16 @@ export const googleRedirect = async (req, res) => {
   const tokenData = await axios({
     url: `https://oauth2.googleapis.com/token`,
     method: "post",
-    data: new URLSearchParams({
+    data: {
       client_id: GOOGLE_CLIENT_ID,
       client_secret: GOOGLE_CLIENT_SECRET,
       redirect_uri: `${BACKEND_URL}}/api/auth/google-redirect`,
       grant_type: "authorization_code",
       code,
-    }),
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
     },
   });
+
+  console.log(tokenData);
 
   const userData = await axios({
     url: "https://www.googleapis.com/oauth2/v2/userinfo",
@@ -52,8 +51,9 @@ export const googleRedirect = async (req, res) => {
     },
   });
 
-  const token = await authGoogle(userData.data);
+  console.log(userData);
 
+  const { token } = await authGoogle(userData.data);
 
-  return res.redirect(`${FRONTEND_URL}/api/auth?token=${token}`);
+  return res.redirect(`${FRONTEND_URL}?token=${token}`);
 };
